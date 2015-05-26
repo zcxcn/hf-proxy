ebdc23ec7c410ac249dfe2294902f5ae832fb3a6
zcxcn <zcxcn@163.com>
2015-05-26 19:53:08 +0800

    进程守护

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
      httpProxy = require('http-proxy'),
      harmon = require('harmon'),
      syspath = require('path'),
-     main = require('./lib/main');
+     main = require('./lib/main'),
+     multiprocess = require('./lib/multiprocess');
  process.on('uncaughtException', function(err) {
      console.log('error:', err);
  });
@@ -22,95 +23,99 @@
  var baselib = syspath.join(module.filename, '../')
  var channelHash = require(baselib + 'lib/channelHash.json');
  var connect = require('connect');
- var app = connect()
-     .use(function(req, res, next) {
-         var pu = req._parsedUrl;
-         if (pu.host.indexOf('qunar.com') > -1) {
-             // if (pu.url.test(/[\.js|\.css|\.png]$/)) {
-             if (/(\.js|\.css|\.png|\.jpg|\.gif|\.swf)(\?.*)?$/.test(req.url)) {
-                 next();
-                 return;
-             }
-             var index = pu.host.indexOf('.')
-             var subDomain = pu.host.substr(0, index);
-
-             //  console.log(req.url);
-
-             var channelInfo = getChannelInfo(subDomain);
-             if (!channelInfo) {
-                 next();
-                 return;
-             }
-             var content = main.getContent(channelInfo);
-             if (!content) {
-                 next();
-                 return;
-             }
-             //获取嵌入脚本
-             getScript(function(script) {
-                 script = script.replace('{{channel.path}}', channelInfo.path);
-
-                 if (isDebug) {
-                     content.html = content.html + '<script>' + script + '</script>';
+ multiprocess.listen(function() {
+     var app = connect()
+         .use(function(req, res, next) {
+             var pu = req._parsedUrl;
+             if (pu.host.indexOf('qunar.com') > -1) {
+                 // if (pu.url.test(/[\.js|\.css|\.png]$/)) {
+                 if (/(\.js|\.css|\.png|\.jpg|\.gif|\.swf)(\?.*)?$/.test(req.url)) {
+                     next();
+                     return;
                  }
-                 //替换html
-                 htmlselect.query = 'div.q_header';
-                 htmlselect.func = function(node) {
-                         node.createWriteStream({
-                             outer: false
-                         }).end(content.html);
+                 var index = pu.host.indexOf('.')
+                 var subDomain = pu.host.substr(0, index);
+
+                 //  console.log(req.url);
+
+                 var channelInfo = getChannelInfo(subDomain);
+                 if (!channelInfo) {
+                     next();
+                     return;
+                 }
+                 var content = main.getContent(channelInfo);
+                 if (!content) {
+                     next();
+                     return;
+                 }
+                 //获取嵌入脚本
+                 getScript(function(script) {
+                     script = script.replace('{{channel.path}}', channelInfo.path);
+
+                     if (isDebug) {
+                         content.html = content.html + '<script>' + script + '</script>';
                      }
-                 //替换style
-                 styleselect.query = 'style[data-hfstamp]';
-                 styleselect.func = function(node) {
+                     //替换html
+                     htmlselect.query = 'div.q_header';
+                     htmlselect.func = function(node) {
+                             node.createWriteStream({
+                                 outer: false
+                             }).end(content.html);
+                         }
+                         //替换style
+                     styleselect.query = 'style[data-hfstamp]';
+                     styleselect.func = function(node) {
+                             node.createWriteStream({
+                                 outer: true
+                             }).end(content.style);
+                         }
+                         //替换sidebarhtml
+                     sidebarHTMLSelect.query = '.q_ucsidebar ul';
+                     sidebarHTMLSelect.func = function(node) {
+                             node.createWriteStream({
+                                 outer: true
+                             }).end(content.sidebarHTML);
+                         }
+                         //替换sidebarstyle
+                     sidebarStyleSelect.query = '.q_ucsidebar style';
+                     sidebarStyleSelect.func = function(node) {
                          node.createWriteStream({
                              outer: true
-                         }).end(content.style);
+                         }).end(content.sidebarStyle);
                      }
-                 //替换sidebarhtml
-                 sidebarHTMLSelect.query = '.q_ucsidebar ul';
-                 sidebarHTMLSelect.func = function(node) {
-                         node.createWriteStream({
-                             outer: true
-                         }).end(content.sidebarHTML);
-                     }
-                 //替换sidebarstyle
-                 sidebarStyleSelect.query = '.q_ucsidebar style';
-                 sidebarStyleSelect.func = function(node) {
-                     node.createWriteStream({
-                         outer: true
-                     }).end(content.sidebarStyle);
-                 }
-                 selects.push(htmlselect);
-                 selects.push(styleselect);
-                 selects.push(sidebarHTMLSelect);
-                 selects.push(sidebarStyleSelect);
-                 var func = harmon([], selects, true);
-                 func(req, res, next);
+                     selects.push(htmlselect);
+                     selects.push(styleselect);
+                     selects.push(sidebarHTMLSelect);
+                     selects.push(sidebarStyleSelect);
+                     var func = harmon([], selects, true);
+                     func(req, res, next);
+                 });
+
+             } else {
+                 next();
+             }
+         })
+         .use(function(req, res) {
+             var protocol = req._parsedUrl.protocol;
+             var host = req.headers.host;
+             console.log(protocol + '//' + host)
+             proxy.web(req, res, {
+                 target: protocol + '//' + host
+             }, function(e) {
+                 //避免刷太快报 socket hang up
+                 console.log('proxy error:', e);
              });
+         })
+         .listen(8001);
+     app.on('error', function(e) {
+         console.log('connect error:', e);
+     })
+     var proxy = httpProxy.createProxyServer({});
 
-         } else {
-             next();
-         }
-     })
-     .use(function(req, res) {
-         var protocol = req._parsedUrl.protocol;
-         var host = req.headers.host;
-         console.log(protocol + '//' + host)
-         proxy.web(req, res, {
-             target: protocol + '//' + host
-         }, function(e) {
-             //避免刷太快报 socket hang up
-             console.log('proxy error:', e);
-         });
-     })
-     .listen(8001);
- app.on('error', function(e) {
-     console.log('connect error:', e);
+     console.log(8001);
+ }, {
+     numCPUs: 8
  })
- var proxy = httpProxy.createProxyServer({});
-
- console.log(8001);
 
  function getScript(callback) {
      if (scriptCache) {
